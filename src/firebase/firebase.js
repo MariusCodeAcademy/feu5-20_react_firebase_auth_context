9da171ceaf37991ef0cd6e10a978359226d0b509
Marius CA <mariuskcodeacademy@gmail.com>
2023-04-14 12:02:14 +0300

    get posts

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,6 +4,7 @@ import { initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -22,3 +23,7 @@ const app = initializeApp(firebaseConfig);
 // exportuoti auth constanta https://firebase.google.com/docs/auth/web/start
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
+
+// Initialize Cloud Firestore and get a reference to the service
+// https://firebase.google.com/docs/firestore/quickstart#web-version-9
+export const db = getFirestore(app);
--- src/pages/PostsPage.jsx
+++ src/pages/PostsPage.jsx
@@ -1,8 +1,40 @@
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../firebase/firebase';
+import { useEffect, useState } from 'react';
+
 function PostsPage() {
+  const [postsArr, setPostsArr] = useState([]);
+
+  useEffect(() => {
+    async function getPosts() {
+      // norim gauti postus
+      const querySnapshot = await getDocs(collection(db, 'posts'));
+      const tempPosts = [];
+      querySnapshot.forEach((doc) => {
+        // console.log(`${doc.id} => ${doc.data()}`);
+        // console.log('doc.data() ===', doc.data());
+        tempPosts.push({
+          uid: doc.id,
+          ...doc.data(),
+        });
+      });
+      console.log('tempPosts ===', tempPosts);
+      setPostsArr(tempPosts);
+    }
+    getPosts();
+  }, []);
+
   return (
     <div className="container">
       <h1>PostsPage</h1>
       <p>This is PostsPage</p>
+      {/* map over postsArr and display title and body of each post */}
+      <ul>
+        <li>post 1</li>
+        <li>post 1</li>
+        <li>post 1</li>
+        <li>post 1</li>
+      </ul>
     </div>
   );
 }
