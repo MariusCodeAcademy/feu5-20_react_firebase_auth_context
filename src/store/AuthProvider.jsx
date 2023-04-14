e1cec0ab801fc9bbf3bc283fa29481edad9cbe13
Marius CA <mariuskcodeacademy@gmail.com>
2023-04-14 10:44:54 +0300

    AuthProvider + ctx

---
+++ src/store/AuthProvider.jsx
@@ -0,0 +1,24 @@
+import { createContext, useState } from 'react';
+
+const AuthContext = createContext({
+  user: {},
+  isLoading: false,
+  login() {},
+  logout() {},
+  register() {},
+});
+
+function AuthProvider({ children }) {
+  const [user, setUser] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  const authCtx = {
+    user,
+    isLoading,
+  };
+  return (
+    <AuthContext.Provider value={authCtx}>{children}</AuthContext.Provider>
+  );
+}
+
+export default AuthProvider;
