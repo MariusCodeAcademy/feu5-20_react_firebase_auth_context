af8ba5fe1efa92eb2342c93a7ccc19265c0a9abd
Marius CA <mariuskcodeacademy@gmail.com>
2023-04-17 10:52:28 +0300

    singe post tags

--- src/components/posts/SinglePost.jsx
+++ src/components/posts/SinglePost.jsx
@@ -3,18 +3,22 @@ import Card from '../ui/card/Card';
 import './SinglePost.scss';
 
 function SinglePost({ item }) {
-  console.log('item ===', item.date);
+  // console.log('item ===', item.date);
 
   // atvaizduoti tagus kaip tagus (pill shape)
+  const tagArr = item.tags.split(',').map((t) => t.trim());
+
   return (
     <li className="singlePost">
       <Card>
         <h3>{item.title}</h3>
         <p className="author">{item.author}</p>
         <p>{item.body}</p>
+        <h4 className="tagTitle">Tags:</h4>
         <p className="tags">
-          <span>tag1</span>
-          <span>tag2</span>
+          {tagArr.map((tag) => (
+            <span key={tag}>{tag}</span>
+          ))}
         </p>
         <hr />
         <p className="date">{item.date}</p>
