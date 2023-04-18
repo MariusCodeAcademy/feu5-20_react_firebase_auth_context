d337744843c6bedad985e73ac2f34eab36952590
Marius CA <mariuskcodeacademy@gmail.com>
2023-04-18 11:44:59 +0300

    protected route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './styles/reset.css';
 import './styles/App.css';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import ProfilePage from './pages/ProfilePage';
@@ -10,7 +10,10 @@ import PostsPage from './pages/PostsPage';
 import AddPostsPage from './pages/AddPostpage';
 import SinglePostPage from './pages/SinglePostPage';
 import { Toaster } from 'react-hot-toast';
+import { useAuthCtx } from './store/AuthProvider';
+
 function App() {
+  const { isLoggedIn } = useAuthCtx();
   return (
     <div className="">
       <Toaster />
@@ -18,7 +21,11 @@ function App() {
       <Feedback />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
+        {/* apsaugotas route */}
+        <Route
+          path="/login"
+          element={isLoggedIn ? <Navigate to={'/profile'} /> : <LoginPage />}
+        />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/posts" element={<PostsPage />} />
         <Route path="/posts/new" element={<AddPostsPage />} />
--- src/pages/SinglePostPage.jsx
+++ src/pages/SinglePostPage.jsx
@@ -1,11 +1,21 @@
-import { doc, getDoc, collection, getDocs, query } from 'firebase/firestore';
+import {
+  doc,
+  getDoc,
+  collection,
+  getDocs,
+  query,
+  deleteDoc,
+} from 'firebase/firestore';
 import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { db } from '../firebase/firebase';
 import Card from '../components/ui/card/Card';
 import CommentBlock from '../components/comments/CommentsBlock';
+import { toast } from 'react-hot-toast';
+import { useAuthCtx } from '../store/AuthProvider';
 
 function SinglePostPage() {
+  const { user } = useAuthCtx();
   const { postUid } = useParams();
   const navigate = useNavigate();
   const [postObj, setPostObj] = useState({});
@@ -29,10 +39,26 @@ function SinglePostPage() {
   }, []);
   // parisiusti posta is posts //https://firebase.google.com/docs/firestore/query-data/get-data?hl=en&authuser=0#get_a_document
 
-  function deleteFirePost() {
+  function deleteFirePost(uid) {
     // https://firebase.google.com/docs/firestore/manage-data/delete-data?hl=en&authuser=2#delete_documents
     // istrinti dokumenta
+    const delPromise = deleteDoc(doc(db, 'posts', uid))
+      .then(() => {
+        console.log('istrinta');
+        navigate('/posts');
+        toast.success('istrinta');
+      })
+      .catch((error) => {
+        console.warn('error ===', error);
+        toast.error('klaida trinant');
+      });
     // atvaizduoti eiga su toast.promise
+    // kazkodel neatvaizdavo klaidos busenos
+    // toast.promise(delPromise, {
+    //   loading: 'Deleting',
+    //   success: 'Delete success',
+    //   error: 'Error when Deleting',
+    // });
   }
 
   return (
@@ -41,7 +67,9 @@ function SinglePostPage() {
         <h1>{postObj.title}</h1>
         <p>This is SinglePostPage</p>
         <button onClick={() => navigate(-1)}>Go back</button>
-        <button>Delete Post</button>
+        {user.uid === postObj.userUid && (
+          <button onClick={() => deleteFirePost(postUid)}>Delete Post</button>
+        )}
       </Card>
       {/* Comments Block */}
       <CommentBlock postId={postUid} />
